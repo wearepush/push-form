27148d377b2408d0a7a95494f84e716f4849ffcf
Iuriy-Budnikov <iuriy.budnikov@gmail.com>
2022-01-07 18:02:04 +0200

    init travis

--- config/travis.exs
+++ config/travis.exs
@@ -4,11 +4,11 @@ import Config
 # you can enable the server option below.
 config :form, FormWeb.Endpoint,
   http: [port: 4002],
+  secret_key_base: "FQFcpK3/SPOwvofMHdG2jbScpKOTzgQfruTiHviuv57UyXX0gS8d6Aw9fVdSW7iF",
   server: false
 
 # Configure your database
 config :form, Form.Repo,
-  # adapter: Ecto.Adapters.Postgres,
   username: "postgres",
   password: "",
   database: "form_test",
