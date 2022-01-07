3e741cc7db566b0d23810039b901e1443109706b
Iuriy-Budnikov <iuriy.budnikov@gmail.com>
2022-01-07 17:11:13 +0200

    init travis

--- test/form_web/controllers/page_controller_test.exs
+++ test/form_web/controllers/page_controller_test.exs
@@ -2,9 +2,6 @@ defmodule FormWeb.PageControllerTest do
   use FormWeb.ConnCase
 
   test "GET /", %{conn: conn} do
-
-    IO.inspect System.get_env("SECRET_KEY_BASE")
-
     conn = get(conn, "/")
     assert html_response(conn, 200) =~ "Welcome to Phoenix!"
   end
