3cb8a62980e87f5e4bda07e53a61ef7b4e3cf314
Iuriy-Budnikov <iuriy.budnikov@gmail.com>
2022-01-07 16:42:03 +0200

    init travis

--- config/travis.exs
+++ config/travis.exs
@@ -4,6 +4,7 @@ use Mix.Config
 # you can enable the server option below.
 config :form, Form.Endpoint,
   http: [port: 4001],
+  secret_key_base: "FQFcpK3/SPOwvofMHdG2jbScpKOTzgQfruTiHviuv57UyXX0gS8d6Aw9fVdSW7iF",
   server: false
 
 # Print only warnings and errors during test
