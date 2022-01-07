f7c6b191df802014b8a0bfe9210d34e08da105e8
Iuriy-Budnikov <iuriy.budnikov@gmail.com>
2022-01-07 17:01:36 +0200

    init travis

--- config/travis.exs
+++ config/travis.exs
@@ -2,12 +2,12 @@ import Config
 
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
-# config :form, Form.Endpoint,
-#   http: [port: 4001],
-#   server: false
+config :form, Form.Endpoint,
+  http: [port: 4001],
+  server: false
 
 # Print only warnings and errors during test
-# config :logger, level: :warn
+config :logger, level: :warn
 
 # Configure your database
 config :form, Form.Repo,
