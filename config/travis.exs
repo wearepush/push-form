f570fbc4ede3c9df1fd165d921e8e3c4985bce16
Iuriy-Budnikov <iuriy.budnikov@gmail.com>
2022-01-07 16:48:46 +0200

    init travis

--- config/travis.exs
+++ config/travis.exs
@@ -1,14 +1,13 @@
-use Mix.Config
+import Config
 
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
-config :form, Form.Endpoint,
-  http: [port: 4001],
-  secret_key_base: "FQFcpK3/SPOwvofMHdG2jbScpKOTzgQfruTiHviuv57UyXX0gS8d6Aw9fVdSW7iF",
-  server: false
+# config :form, Form.Endpoint,
+#   http: [port: 4001],
+#   server: false
 
 # Print only warnings and errors during test
-config :logger, level: :warn
+# config :logger, level: :warn
 
 # Configure your database
 config :form, Form.Repo,
